9c6b97b92614ddf8cea48a8e2876a4f59a781fe6
Tempate <ddiazq.2012091@lsmaravillas.com>
2017-11-12 23:43:14 +0100

    1.0

--- projects/settings.py
+++ projects/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
 ]
 
 INSTALLED_APPS += [
+    "whitenoise"
+]
+
+INSTALLED_APPS += [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
