abc93c5c9af2cc714441794138952c9db67a6a74
tempate <ddiazq28@gmail.com>
2025-04-11 20:04:02 +0300

    Fix incorrect use of 'prompt.settings'

--- projects/wsgi.py
+++ projects/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prompt.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projects.settings")
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
