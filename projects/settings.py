a777840dfd1faff1e2f5072edb8cd15803cd1330
Daniel Díaz Quílez <33602217+Tempate@users.noreply.github.com>
2025-04-11 20:51:40 +0300

    Update for production (#3)
    * Update requirements
    
    * Fix incorrect use of 'prompt.settings'
    
    * Support Render's link
    
    * Remove SECRET_KEY
    
    * Generate a secret key
    
    * Add new steps to README

--- projects/settings.py
+++ projects/settings.py
@@ -8,14 +8,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vjcpktgm(zx8pjz5k6vo1bb4iz7_!g+gdd3(uc&nbus8@_yrwj'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
-    "tempate.herokuapp.com"
+    "danidiaz.onrender.com"
 ]
 
 INSTALLED_APPS = [
